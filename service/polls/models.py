ad698b77e95183adaea1d2670f311de8970247a3
ldh1593894823 <1593894823@qq.com>
2022-12-11 15:33:37 +0800

    代码同步

--- service/polls/models.py
+++ service/polls/models.py
@@ -10,7 +10,9 @@ class Adminster(models.Model): # 超级管理员
     username = models.CharField(max_length=32)  # 用户名
     password = models.CharField(max_length=32)  # 密码
 
-
+#公告
+class Comments(models.Model):
+    comment = models.CharField(max_length=64)  # 公告
 
 # 普通用户
 class User(models.Model): 
@@ -19,7 +21,7 @@ class User(models.Model):
     userphone = models.CharField(max_length=32)  # 手机号登录用
     user_qq = models.CharField(max_length=32)  # 用户qq号码
 
-# 所有商品
+# 商品列表
 class Shopping(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
     name = models.CharField(max_length=64)  # 商品名称
@@ -30,3 +32,8 @@ class Shopping(models.Model):
     category = models.CharField(max_length=32)  # 商品分类
     myimage = models.CharField(max_length=256)  # 商品图片
 
+#购物车
+class Shop_cart(models.Model):
+    user_id = models.CharField(max_length=64)  #用户id
+    shop_num = models.CharField(max_length=16) #加购数量
+    shop_id = models.CharField(max_length=64)  #商品id
